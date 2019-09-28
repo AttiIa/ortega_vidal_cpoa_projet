0db1409d00b60538a054f2ca91f4c05412f282fc
Jeremie Ortega <ortega17u@iutm-inf-f33-17.iutmetz.site.univ-lorraine.fr>
2019-09-28 10:27:44 +0200

    modification factory

--- cpoa_td2_dao/src/dao/factory/MySQLDAOFactory.java
+++ cpoa_td2_dao/src/dao/factory/MySQLDAOFactory.java
@@ -1,14 +1,30 @@
 package dao.factory;
 
-import dao.interfaces.*;
+import dao.interfaces.AbonnementDAO;
+import dao.interfaces.ClientDAO;
+import dao.interfaces.PeriodiciteDAO;
+import dao.interfaces.RevueDAO;
 
-public class MySQLDAOFactory extends DAOFactory{
+public class MySQLDAOFactory extends DAOFactory {
+
 	@Override
 	public PeriodiciteDAO getPeriodiciteDAO() {
-		return null; //MySQLPeriodiciteDAO.getInstance();
+		return null;
 	}
+
 	@Override
-	public AbonnementDAO getAbonnementDAO(){
-		return null;// MySQLPromoDAO.getInstance();
+	public AbonnementDAO getAbonnementDAO() {
+		return null;
 	}
+
+	@Override
+	public ClientDAO getClientDAO() {
+		return null;
+	}
+
+	@Override
+	public RevueDAO getRevueDAO() {
+		return null;
+	}
+
 }
