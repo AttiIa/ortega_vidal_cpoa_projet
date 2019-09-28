9ee428536c96f04676b5954e863baf08dcca977a
Thomas Vidal <vidal27u@iutm-inf-f33-16.iutmetz.site.univ-lorraine.fr>
2019-09-28 10:50:15 +0200

    modif list memoire

--- cpoa_td2_dao/dao/dao/factory/ListeMemoireDAOFactory.java
+++ cpoa_td2_dao/dao/dao/factory/ListeMemoireDAOFactory.java
@@ -1,23 +1,28 @@
 package dao.factory;
 
 import dao.interfaces.*;
+import dao.mysql.MySQLAbonnementDAO;
+import dao.mysql.MySQLClientDAO;
+import dao.mysql.MySQLPeriodiciteDAO;
+import dao.mysql.MySQLRevueDAO;
 
 public class ListeMemoireDAOFactory extends DAOFactory{
-	
+	@Override
 	public PeriodiciteDAO getPeriodiciteDAO() {
-		return null;
+		return MySQLPeriodiciteDAO.getInstance();
 	}
-
-	public AbonnementDAO getPAbonnementDAO() {
-		return null;
+	@Override
+	public AbonnementDAO getAbonnementDAO() {
+		return  MySQLAbonnementDAO.getInstance();
 	}
-
+	@Override
 	public ClientDAO getClientDAO() {
-		return null;
+		return  MySQLClientDAO.getInstance();
+	}
+	@Override
+	public RevueDAO getRevueDAO() {
+		return  MySQLRevueDAO.getInstance();
 	}
 
-	public RevueDAO getRevueDAO() {
-		return null;
-	}
 
 }
