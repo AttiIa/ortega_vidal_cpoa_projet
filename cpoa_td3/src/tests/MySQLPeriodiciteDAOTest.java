c4fda39a889a4176dfedaa3076f197b6fa4e3c74
Jeremie Ortega <ortega17u@iutm-inf-f33-17.iutmetz.site.univ-lorraine.fr>
2019-10-05 09:47:57 +0200

    creation des tests MySQL

---
+++ cpoa_td3/src/tests/MySQLPeriodiciteDAOTest.java
@@ -0,0 +1,34 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MySQLPeriodiciteDAOTest {
+
+	@Test
+	void testCreate() {
+		fail("Pas encore implémenté");
+	}
+
+	@Test
+	void testDelete() {
+		fail("Pas encore implémenté");
+	}
+
+	@Test
+	void testUpdate() {
+		fail("Pas encore implémenté");
+	}
+
+	@Test
+	void testGetById() {
+		fail("Pas encore implémenté");
+	}
+
+	@Test
+	void testFindAll() {
+		fail("Pas encore implémenté");
+	}
+
+}
