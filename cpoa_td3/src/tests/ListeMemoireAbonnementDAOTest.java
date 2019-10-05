e3a7f8e4124e926beacbeb14d0a8aa42a2ed1dc0
Jeremie Ortega <ortega17u@iutm-inf-f33-17.iutmetz.site.univ-lorraine.fr>
2019-10-05 11:45:51 +0200

    squelette ListeMemoireTests

---
+++ cpoa_td3/src/tests/ListeMemoireAbonnementDAOTest.java
@@ -0,0 +1,39 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ListeMemoireAbonnementDAOTest {
+
+	@Test
+	void testCreate() {
+		fail("Pas encore implémenté");
+	}
+
+	@Test
+	void testDelete() {
+		fail("Pas encore implémenté");
+	}
+
+	@Test
+	void testUpdate() {
+		fail("Pas encore implémenté");
+	}
+
+	@Test
+	void testGetByIdInt() {
+		fail("Pas encore implémenté");
+	}
+
+	@Test
+	void testGetByIdIntInt() {
+		fail("Pas encore implémenté");
+	}
+
+	@Test
+	void testFindAll() {
+		fail("Pas encore implémenté");
+	}
+
+}
---
+++ cpoa_td3/src/tests/ListeMemoireClientDAOTest.java
@@ -0,0 +1,34 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ListeMemoireClientDAOTest {
+
+	@Test
+	void testCreate() {
+		fail("Pas encore implémenté");
+	}
+
+	@Test
+	void testDelete() {
+		fail("Pas encore implémenté");
+	}
+
+	@Test
+	void testUpdate() {
+		fail("Pas encore implémenté");
+	}
+	
+	@Test
+	void testGetByIdInt() {
+		fail("Pas encore implémenté");
+	}
+
+	@Test
+	void testFindAll() {
+		fail("Pas encore implémenté");
+	}
+
+}
