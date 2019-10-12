d4eb784dc80c4c4ba52811b16b834b642621b61e
vidal27u <vidal27u@iutm-inf-f33-16.iutmetz.site.univ-lorraine.fr>
2019-10-12 11:13:51 +0200

    main corrigé

--- cpoa_td4_javafx/src/main/mainInterface.java
+++ cpoa_td4_javafx/src/main/mainInterface.java
@@ -1,29 +1,36 @@
 package main;
 
-import javax.print.DocFlavor.URL;
+import java.net.URL;
 
 import javafx.fxml.FXMLLoader;
-import javafx.scene.*;
-import javafx.stage.*;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class mainInterface {
 
-
 	public void start(Stage primaryStage) {
-	try {
-	java.net.URL fxmlURL=getClass().getResource("javafx/interface_graphique_revue.fxml");
-	FXMLLoader fxmlLoader = new FXMLLoader(fxmlURL);
-	Node root = fxmlLoader.load();
-	Scene scene = new Scene((VBox) root, 640, 400);
-	scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-	primaryStage.setScene(scene);
-	primaryStage.setTitle("Ma première fenêtre JavaFX");
-	primaryStage.show();
-	} catch (Exception e) {
-	e.printStackTrace();
+		try {
+			URL fxmlURL = getClass().getResource("javafx/fenetre.fxml");
+			FXMLLoader fxmlLoader = new FXMLLoader(fxmlURL);
+			Node root = fxmlLoader.load();
+			Scene scene = new Scene((VBox) root, 640, 400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Ma première fenêtre JavaFX");
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	}    
+
 	public static void main(String[] args) {
 		launch(args);
-		}
+	}
+
+	private static void launch(String[] args) {
+		// TODO Stub de la méthode généré automatiquement
+		
+	}
 }
