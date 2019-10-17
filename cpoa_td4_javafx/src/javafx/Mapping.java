479a2c9b95e1e630fefb11706d86890476a17d30
vidal <vidal@DESKTOP-48RVJ85.home>
2019-10-17 19:42:04 +0200

    reste erreur du run

--- cpoa_td4_javafx/src/javafx/Mapping.java
+++ cpoa_td4_javafx/src/javafx/Mapping.java
@@ -1,33 +1,60 @@
 package javafx;
-
+import dao.metier.Revue;
 import dao.metier.Periodicite;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import dao.factory.DAOFactory;
+import dao.factory.Persistance;
+import javafx.fxml.Initializable;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.scene.paint.Color;
 import javafx.scene.control.*;
 
-public class Mapping {
-	
+public class Mapping implements Initializable {
+	//@FXML
+	private TextField id_revue;
 	@FXML
 	private TextField titre;
+	
 	@FXML
 	private TextField tarif;
 	@FXML
 	private ComboBox<Periodicite> libelle;
+	
 	@FXML
 	private TextArea description;
+	
 	@FXML
 	private TextField visuel;
+	
 	@FXML
 	private Button creer;
 	@FXML
 	private Label affichage;
 	@FXML
-	private RadioButton choix;
+	private RadioButton mysql;
+	@FXML
+	private RadioButton list;
 	
 	@Override
 	public String toString() {
 		return "Mapping [titre=" + titre + ", tarif=" + tarif + ", libelle=" + libelle + ", description=" + description
-				+ ", visuel=" + visuel + ", choix=" + choix + "]";
+				+ ", visuel=" + visuel + ", mysql=" + mysql +", list=" + list + "]";
 	}
+	
+	 public void initialize(URL location, ResourceBundle resources) {
+
+	        DAOFactory dao = DAOFactory.getDAOFactory(Persistance.ListeMemoire);
+
+	        try {
+				this.libelle.setItems(FXCollections.observableArrayList(dao.getPeriodiciteDAO().findAll()));
+			} catch (Exception e) {
+				System.out.println("Problème à l'initialisation ");
+				e.printStackTrace();
+			}
+	    }
 	
 	public void Init() {
 		titre.setText("");
@@ -39,8 +66,32 @@ public class Mapping {
 	
 	@FXML
 	public void create() {
-		this.affichage.setText("appuie sur bouton");
+		Periodicite period = libelle.getValue();
+		
+		if ((id_revue.getText().isEmpty()) || (titre.getText().isEmpty()) || (description.getText().isEmpty()) || (tarif.getText().isEmpty()) || (visuel.getText().isEmpty()) || (period == null) ){
+			affichage.setTextFill(Color.web("red"));
+			this.affichage.setText("Les champs ne sont pas tous valides");
+		}
+		
+		else {	
+			try {
+				double txt_tarif = Double.parseDouble(tarif.getText());
+				affichage.setTextFill(Color.web("#000000"));
+				affichage.setText(toString());
+				int txt_revue = Integer.parseInt(id_revue.getText());
+				String txt_titre = titre.getText();
+				String txt_description = description.getText();
+				String txt_visuel = visuel.getText();
+				Revue rev = new Revue(txt_revue, txt_titre, txt_description, txt_tarif, txt_visuel, period.getId_periodicite());
+		        DAOFactory daos = DAOFactory.getDAOFactory(Persistance.MySQL);
+		        daos.getRevueDAO().create(rev);
+				}
+				catch (Exception e) {
+				affichage.setTextFill(Color.web("red"));
+				affichage.setText(toString());
+				}
+		//this.affichage.setText("appuie sur bouton");
 	}
 	
 	
-}
+}}
