447dbe6b6096f7b677b384cc55cecb156e5706d0
vidal <vidal@DESKTOP-48RVJ85.home>
2019-10-17 20:02:33 +0200

    rajout id_revue

--- cpoa_td4_javafx/src/main/mainInterface.java
+++ cpoa_td4_javafx/src/main/mainInterface.java
@@ -10,13 +10,13 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class mainInterface extends Application{
-
+	@Override
 	public void start(Stage primaryStage) {
 		try {
 			URL fxmlURL = getClass().getResource("fenetre.fxml");
 			FXMLLoader fxmlLoader = new FXMLLoader(fxmlURL);
 			Node root = fxmlLoader.load();
-			Scene scene = new Scene((VBox) root, 640, 400);
+			Scene scene = new Scene((VBox) root, 600, 400);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Ma premiere fenetre JavaFX");
 			primaryStage.show();
