1a449b44a1dcdbbe2f81362cb150407d4ecff0c6
vidal27u <vidal27u@iutm-inf-f33-16.iutmetz.site.univ-lorraine.fr>
2019-10-19 11:54:26 +0200

    erreur

--- cpoa_projet/src/main/Mapping.java
+++ cpoa_projet/src/main/Mapping.java
@@ -1,29 +1,39 @@
 package main;
-import dao.metier.Revue;
-import dao.metier.Periodicite;
+
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import dao.factory.DAOFactory;
 import dao.factory.Persistance;
-import javafx.fxml.Initializable;
+import dao.metier.Periodicite;
+import dao.metier.Revue;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.paint.Color;
-import javafx.scene.control.*;
 
 public class Mapping implements Initializable {
-	
+
 	@FXML
-	private TextField titre;	
+	private TextField titre;
 	@FXML
 	private TextField tarif;
 	@FXML
-	private ComboBox<Periodicite> libelle;	
+	private ComboBox<Periodicite> libelle;
 	@FXML
-	private TextArea description;	
+	private TextArea description;
 	@FXML
-	private TextField visuel;	
+	private TextField visuel;
 	@FXML
 	private Button creer;
 	@FXML
@@ -41,41 +51,68 @@ public class Mapping implements Initializable {
 	@FXML
 	private TableView<Revue> tblRevue;
 
-	
+	public void tblRevue() {
+		this.tblRevue = new TableView<>();
+
+		TableColumn<Revue, String> colIdRevue = new TableColumn<>("id_revue");
+		TableColumn<Revue, String> colTitre = new TableColumn<>("colTitre");
+		TableColumn<Revue, String> colDescription = new TableColumn<>("colDescription");
+		TableColumn<Revue, String> colTarif = new TableColumn<>("colTarif");
+		TableColumn<Revue, String> colPeriodicite = new TableColumn<>("colPeriodicite");
+		TableColumn<Revue, String> colVisuel = new TableColumn<>("colVisuel");
+
+		colIdRevue.setCellValueFactory(new PropertyValueFactory<Revue, String>("id_revue"));
+		colTitre.setCellValueFactory(new PropertyValueFactory<Revue, String>("titre"));
+		colDescription.setCellValueFactory(new PropertyValueFactory<Revue, String>("description"));
+		colTarif.setCellValueFactory(new PropertyValueFactory<Revue, String>("tarif_numero"));
+		colPeriodicite.setCellValueFactory(new PropertyValueFactory<Revue, String>("id_periodicite"));
+		colVisuel.setCellValueFactory(new PropertyValueFactory<Revue, String>("visuel"));
+
+		this.tblRevue.getColumns().setAll(colIdRevue, colTitre, colDescription, colTarif, colPeriodicite, colVisuel);
+
+		List<Revue> revues = DAOFactory.getDAOFactory(Persistance.MySQL).getRevueDAO().findAll();
+
+		this.tblRevue.getItems().addAll(revues);
+		System.out.println(this.tblRevue.getItems().size());
+		
+		this.tblRevue.refresh();
+
+	}
+
 	@Override
 	public String toString() {
 		return "Ajout de : " + titre.getText() + " (" + tarif.getText() + ")";
 	}
-	
-	 public void initialize(URL location, ResourceBundle resources) {
 
-	        DAOFactory dao = DAOFactory.getDAOFactory(Persistance.ListeMemoire);
+	public void initialize(URL location, ResourceBundle resources) {
 
-	        try {
-				this.libelle.setItems(FXCollections.observableArrayList(dao.getPeriodiciteDAO().findAll()));
-			} catch (Exception e) {
-				System.out.println("Probl�me � l'initialisation ");
-				e.printStackTrace();
-			}
-	    }   
+		DAOFactory dao = DAOFactory.getDAOFactory(Persistance.ListeMemoire);
 	
+		try {
+			this.libelle.setItems(FXCollections.observableArrayList(dao.getPeriodiciteDAO().findAll()));
+			tblRevue();
+		} catch (Exception e) {
+			System.out.println("Probl�me � l'initialisation ");
+			e.printStackTrace();
+		}
+	}
+
 	@FXML
 	public void create() {
-		
+
 		Periodicite period = libelle.getValue();
-		
-		if ((titre.getText().isEmpty()) || (description.getText().isEmpty()) || (tarif.getText().isEmpty()) 
-				|| (visuel.getText().isEmpty()) || (period == null) || ((mysql==null) && (list==null))){
+
+		if ((titre.getText().isEmpty()) || (description.getText().isEmpty()) || (tarif.getText().isEmpty())
+				|| (visuel.getText().isEmpty()) || (period == null) || ((mysql == null) && (list == null))) {
 			affichage.setTextFill(Color.web("red"));
 			this.affichage.setText("Les champs ne sont pas tous valides");
 		}
-		
-		else {	
+
+		else {
 			DAOFactory daos = null;
-			if(mysql.isSelected()) {
+			if (mysql.isSelected()) {
 				daos = DAOFactory.getDAOFactory(Persistance.MySQL);
-			}
-			else if(list.isSelected()){
+			} else if (list.isSelected()) {
 				daos = DAOFactory.getDAOFactory(Persistance.ListeMemoire);
 			}
 			try {
@@ -85,20 +122,26 @@ public class Mapping implements Initializable {
 				String txt_titre = titre.getText();
 				String txt_description = description.getText();
 				String txt_visuel = visuel.getText();
-				
-		        daos.getRevueDAO().create(new Revue(txt_titre, txt_description, txt_tarif, txt_visuel, period.getId_periodicite()));  
-			} 
-			catch (Exception e) {
+
+				daos.getRevueDAO().create(
+						new Revue(txt_titre, txt_description, txt_tarif, txt_visuel, period.getId_periodicite()));
+			} catch (Exception e) {
 				affichage.setTextFill(Color.web("red"));
 				this.affichage.setText("Erreur lors de la creation");
-			} 
-		}	
+			}
+		}
 	}
+
 	@FXML
-	public void update() {}
+	public void update() {
+	}
+
 	@FXML
-	public void delete() {}
+	public void delete() {
+	}
+
 	@FXML
-	public void back() {}
+	public void back() {
+	}
 
 }
