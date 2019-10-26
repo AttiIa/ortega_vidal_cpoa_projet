0999d4c10b1a98bdb3cf69f9a29c43345565eddd
Jeremie Ortega <ortega17u@iutm-inf-f33-17.iutmetz.site.univ-lorraine.fr>
2019-10-26 09:02:53 +0200

    alert

--- cpoa_projet/src/main/Mapping.java
+++ cpoa_projet/src/main/Mapping.java
@@ -161,7 +161,7 @@ public class Mapping implements Initializable {
 		
 		List<Revue> revues = DAOFactory.getDAOFactory(Persistance.MySQL).getRevueDAO().findAll();
 		tblRevue.getItems().clear();
-		this.tblRevue.getItems().addAll(revues);
+		this.tblRevue.getItems().addAll(revues); 
 		
 	
 		
